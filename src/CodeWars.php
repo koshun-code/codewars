7ea5d5fedaeea6e16840dc85c050238e90bb94f2
Koshun-code <sit.com.k@gmail.com>
2021-06-19 19:20:31 +0500

    new one task

--- src/CodeWars.php
+++ src/CodeWars.php
@@ -143,3 +143,32 @@ function tribonachi($signature, $n)
     }
     return $res;
 }
+
+/*
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+ */
+function generateHashtag($str)
+{
+  $str = trim($str);
+  $words = explode(" ", $str);
+  $upperWords = array_map(function($word){
+      return ucfirst($word);
+  }, $words);
+  $newString = implode('',$upperWords);
+  if ($newString === '' || strlen($newString) >= 140) {
+    return false;
+  }
+  return "#{$newString}";
+}
